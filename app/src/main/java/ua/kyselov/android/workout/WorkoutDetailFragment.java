90705b840d37b60a6d9186ff42879ce0d4211184
Ivan Kyselov <kyselovivan@gmail.com>
2016-05-19 18:46:30 +0300

    add WorkoutDetailFragment

---
+++ app/src/main/java/ua/kyselov/android/workout/WorkoutDetailFragment.java
@@ -0,0 +1,33 @@
+package ua.kyselov.android.workout;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutDetailFragment extends Fragment {
+
+    private long workoutId;
+
+    public void setWorkout(long id){
+        this.workoutId = id;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+
+//    public WorkoutDetailFragment() {
+//        // Required empty public constructor
+//    }
+}
