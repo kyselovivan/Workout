e2b45fb513b639ec4604f77d66eca5d043d92397
Ivan Kyselov <kyselovivan@gmail.com>
2016-05-20 11:41:31 +0300

    add FrameLayout + override itemClicked

--- app/src/main/java/ua/kyselov/android/workout/MainActivity.java
+++ app/src/main/java/ua/kyselov/android/workout/MainActivity.java
@@ -1,18 +1,26 @@
 package ua.kyselov.android.workout;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.WorkoutListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        WorkoutDetailFragment frag = (WorkoutDetailFragment)getFragmentManager().findFragmentById(R.id.detail_frag);
-        frag.setWorkout(1);
     }
 
+    @Override
+    public void itemClicked(long id) {
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        details.setWorkout(id);
+        ft.replace(R.id.fragment_container,details);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+    }
 }
