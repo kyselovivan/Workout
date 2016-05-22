5ff27b8a6f41d139af0eb35756138748194b22de
Ivan Kyselov <kyselovivan@gmail.com>
2016-05-22 15:26:04 +0300

    added runTimer() + overriden lifecycle methods

--- app/src/main/java/ua/kyselov/android/workout/StopwatchFragment.java
+++ app/src/main/java/ua/kyselov/android/workout/StopwatchFragment.java
@@ -2,10 +2,12 @@ package ua.kyselov.android.workout;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -33,8 +35,54 @@ public class StopwatchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stopwatch, container, false);
+        View layout = inflater.inflate(R.layout.fragment_stopwatch, container, false);
+        runTimer(layout);
+        return layout;
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(wasRunning) {
+            running = true;
+        }
+    }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+    public void onClickStop(View view){
+        running = false;
+    }
+    public void onClickReset(){
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer(View view){
+        final TextView timeView = (TextView)view.findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minute = (seconds%3600)/60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours,minute,secs);
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
     }
 
 }
