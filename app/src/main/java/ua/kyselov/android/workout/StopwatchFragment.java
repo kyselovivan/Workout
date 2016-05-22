481528622db98e22fa375eb57f3ed1103560fa6d
Ivan Kyselov <kyselovivan@gmail.com>
2016-05-22 17:49:24 +0300

    implements onClickListener

--- app/src/main/java/ua/kyselov/android/workout/StopwatchFragment.java
+++ app/src/main/java/ua/kyselov/android/workout/StopwatchFragment.java
@@ -1,23 +1,28 @@
 package ua.kyselov.android.workout;
 
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StopwatchFragment extends Fragment {
+public class StopwatchFragment extends Fragment implements View.OnClickListener{
 
     private int seconds;
     private boolean running;
     private boolean wasRunning;
+    Button startButton;
+    Button stopButton;
+    Button resetButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -37,6 +42,15 @@ public class StopwatchFragment extends Fragment {
                              Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_stopwatch, container, false);
         runTimer(layout);
+
+        startButton = (Button)layout.findViewById(R.id.start_button);
+        stopButton = (Button)layout.findViewById(R.id.stop_button);
+        resetButton = (Button)layout.findViewById(R.id.reset_button);
+
+        startButton.setOnClickListener(this);
+        stopButton.setOnClickListener(this);
+        resetButton.setOnClickListener(this);
+
         return layout;
     }
 
@@ -61,7 +75,7 @@ public class StopwatchFragment extends Fragment {
     public void onClickStop(View view){
         running = false;
     }
-    public void onClickReset(){
+    public void onClickReset(View v){
         running = false;
         seconds = 0;
     }
@@ -85,4 +99,15 @@ public class StopwatchFragment extends Fragment {
         });
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.start_button: onClickStart(v);
+                break;
+            case R.id.stop_button: onClickStop(v);
+                break;
+            case R.id.reset_button: onClickReset(v);
+                break;
+        }
+    }
 }
